c788f2268489108f016348eb4c55a236d1c0e0c0
ambtus <github@ambt.us>
2009-06-07 17:49:18 -0700

    ndash

--- app/models/page.rb
+++ app/models/page.rb
@@ -446,7 +446,7 @@ class Page < ActiveRecord::Base
     text = text.gsub(/&[lr]dquo;/, '"')
     text = text.gsub(/&amp;/, "&")
     text = text.gsub(/&hellip;/, "...")
-    text = text.gsub(/&mdash;/, "--")
+    text = text.gsub(/&[mn]dash;/, "--")
     text = text.gsub(/&lt;/, "<")
     text = text.gsub(/&gt;/, ">")
     text.gsub(/ +/, ' ').gsub(/\n+ */, "\n\n").gsub(/\n\n\n\n+/, "\n\n").strip
