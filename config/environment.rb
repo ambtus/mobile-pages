c2659a4a4c7633990b5c68cf9afca7551bc66666
Alice Taylor <alice@sidra.ambt.us>
2009-11-18 02:16:31 +0000

    upgraded gem versions of cucumber and sanitize

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'curb'
   config.gem 'nokogiri', :version => ">=1.3.0"
-  config.gem 'sanitize'
+  config.gem 'sanitize', :version => ">=1.1.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
