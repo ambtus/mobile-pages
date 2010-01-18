8534f2b4d11b3fba6c29408bc8cf7dd41448e76b
Alice Taylor <github@ambt.us>
2010-01-18 00:10:45 +0000

    no newrelic

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,6 @@ Rails::Initializer.run do |config|
   config.gem 'nokogiri', :version => ">=1.4.1"
   config.gem 'sanitize', :version => ">=1.1.0"
   config.gem 'mechanize'
-config.gem "newrelic_rpm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
