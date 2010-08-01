a4d0c4390846ca6f48271b02a895f959d97a979f
Alice Taylor <github@ambt.us>
2010-08-01 21:39:14 +0000

    background pdf creation

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -7,15 +7,17 @@ class PdfsController < ApplicationController
     page = Page.find(params[:page_id])
     font_size = params[:id] || Page::DEFAULT_PDF_FONT_SIZE
     file = page.pdf_file_name(font_size)
-    page.to_pdf(font_size) unless File.exists?(file)
+    unless File.exists?(file)
+      redirect_to new_page_pdf_path, :alert => "Pdf doesn't exist" and return
+    end
     send_file file, :type => Mime::PDF, :disposition => 'inline'
   end
 
   def create
     page = Page.find(params[:page_id])
     font_size = params[:font_size] || Page::DEFAULT_PDF_FONT_SIZE
+    redirect_to page, :notice => "Large files may take a while to process"
     page.to_pdf(font_size)
-    redirect_to page
   end
 
 end
