a04313c39fd869d9ff985ee7eb99261972ba246f
Sidra <alice@alum.mit.edu>
2010-12-24 19:53:13 +0000

    more title errors

--- app/models/page.rb
+++ app/models/page.rb
@@ -118,7 +118,7 @@ class Page < ActiveRecord::Base
   end
 
   def clean_title
-    clean = self.title.gsub('/', '').gsub("'", '').gsub("?", '').gsub(",", '').gsub(":", '').gsub('"', '').gsub("#", '').gsub(" ", "_").gsub("&", "")
+    clean = self.title.gsub('/', '').gsub("'", '').gsub("?", '').gsub(",", '').gsub(":", '').gsub('"', '').gsub("#", '').gsub(" ", "_").gsub("&", "").gsub("(", "").gsub(")", "")
     CGI::escape(clean).gsub('+', ' ').gsub('.', ' ')
   end
 
