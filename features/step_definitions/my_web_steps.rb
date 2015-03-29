a39f15a057924e1a089fdbe3bf6e81f7f0c6fafa
Sidra <alice@alum.mit.edu>
2015-03-29 11:43:52 -0400

    removing unused steps in preparation for fixing deprecation warnings

--- features/step_definitions/my_web_steps.rb
+++ features/step_definitions/my_web_steps.rb
@@ -1,14 +1,8 @@
-When /^I check boxes "([^\"]*)"$/ do |fields|
-  fields.split.each do |field|
-    check(field)
-  end
-end
-
 Then /^"([^"]*)" should be selected in "([^"]*)"$/ do |value, field|
    find_field(field).value.should =~ /#{value}/
 end
 
-Given /^I wait (\d+) second.?$/ do |time|
+Given /^(?:|I )wait (\d+) second.?$/ do |time|
   Kernel::sleep time.to_i
 end
 
@@ -16,27 +10,10 @@ When /^(?:|I )fill in "([^\"]*)" with$/ do |field, value|
   fill_in(field, :with => value)
 end
 
-Then /I should have a "([^"]*)" field$/ do |field|
-  assert has_css?(field)
-end
-
-Then /I should not have a "([^"]*)" field$/ do |field|
+Then /^(?:|I )should not have a "([^"]*)" field$/ do |field|
   assert !has_css?(field)
-end
-
-Then /^I should be visiting "([^"]*)"$/ do |link|
-  current_url.should == link
-end
-
-When(/^I go back$/) do
-  visit request.env['HTTP_REFERER']
 end
 
 Then /^"([^\"]*)" should link to "([^\"]*)"$/ do |link_text, link_url|
   page.find_link(link_text)['href'].should == link_url
-end
-
-
-When /^my variable is "(.*?)"$/ do |var|
-  puts var
 end
