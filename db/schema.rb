aabbc9a569b3342a95186cb912405f601cc7edfe
Alice Brown <alice@alum.mit.edu>
2016-01-09 13:28:41 -0500

    modified database for utf8mb4

--- db/schema.rb
+++ db/schema.rb
@@ -9,76 +9,76 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151229203412) do
+ActiveRecord::Schema.define(version: 20151229203412) do
 
-  create_table "authors", :force => true do |t|
-    t.string "name"
+  create_table "authors", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
-  add_index "authors", ["name"], :name => "author_name", :unique => true
+  add_index "authors", ["name"], name: "author_name", unique: true, using: :btree
 
-  create_table "authors_pages", :id => false, :force => true do |t|
-    t.integer "page_id"
-    t.integer "author_id"
+  create_table "authors_pages", id: false, force: :cascade do |t|
+    t.integer "page_id",   limit: 4
+    t.integer "author_id", limit: 4
   end
 
-  create_table "genres", :force => true do |t|
-    t.string "name"
+  create_table "genres", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
-  add_index "genres", ["name"], :name => "genre_name", :unique => true
+  add_index "genres", ["name"], name: "genre_name", unique: true, using: :btree
 
-  create_table "genres_pages", :id => false, :force => true do |t|
-    t.integer "page_id"
-    t.integer "genre_id"
+  create_table "genres_pages", id: false, force: :cascade do |t|
+    t.integer "page_id",  limit: 4
+    t.integer "genre_id", limit: 4
   end
 
-  create_table "hiddens", :force => true do |t|
-    t.string "name"
+  create_table "hiddens", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
-  add_index "hiddens", ["name"], :name => "hidden_name", :unique => true
+  add_index "hiddens", ["name"], name: "hidden_name", unique: true, using: :btree
 
-  create_table "hiddens_pages", :id => false, :force => true do |t|
-    t.integer "page_id"
-    t.integer "hidden_id"
+  create_table "hiddens_pages", id: false, force: :cascade do |t|
+    t.integer "page_id",   limit: 4
+    t.integer "hidden_id", limit: 4
   end
 
-  create_table "pages", :force => true do |t|
-    t.string   "url"
-    t.string   "title"
-    t.text     "notes"
-    t.integer  "parent_id"
-    t.integer  "position"
+  create_table "pages", force: :cascade do |t|
+    t.string   "url",                  limit: 255
+    t.string   "title",                limit: 255
+    t.text     "notes",                limit: 16777215
+    t.integer  "parent_id",            limit: 4
+    t.integer  "position",             limit: 4
     t.datetime "last_read"
     t.datetime "read_after"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "wordcount"
-    t.string   "size"
-    t.integer  "favorite",             :limit => 2, :default => 10
-    t.integer  "ultimate_parent_id"
-    t.integer  "sanitize_version",     :limit => 2, :default => 1,     :null => false
-    t.string   "cached_genre_string",               :default => "",    :null => false
-    t.integer  "interesting"
-    t.integer  "nice"
-    t.boolean  "uploaded",                          :default => false
-    t.string   "cached_hidden_string",              :default => "",    :null => false
+    t.integer  "wordcount",            limit: 4
+    t.string   "size",                 limit: 255
+    t.integer  "favorite",             limit: 2,        default: 10
+    t.integer  "ultimate_parent_id",   limit: 4
+    t.integer  "sanitize_version",     limit: 2,        default: 1,     null: false
+    t.string   "cached_genre_string",  limit: 255,      default: "",    null: false
+    t.integer  "interesting",          limit: 4
+    t.integer  "nice",                 limit: 4
+    t.boolean  "uploaded",                              default: false
+    t.string   "cached_hidden_string", limit: 255,      default: "",    null: false
   end
 
-  add_index "pages", ["favorite"], :name => "index_pages_on_favorite"
-  add_index "pages", ["parent_id"], :name => "index_pages_on_parent_id"
-  add_index "pages", ["size"], :name => "index_pages_on_size"
-  add_index "pages", ["ultimate_parent_id"], :name => "index_pages_on_ultimate_parent_id"
+  add_index "pages", ["favorite"], name: "index_pages_on_favorite", using: :btree
+  add_index "pages", ["parent_id"], name: "index_pages_on_parent_id", using: :btree
+  add_index "pages", ["size"], name: "index_pages_on_size", length: {"size"=>250}, using: :btree
+  add_index "pages", ["ultimate_parent_id"], name: "index_pages_on_ultimate_parent_id", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "auth_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "auth_token",      limit: 255
   end
 
 end
