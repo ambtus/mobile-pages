bf5fb831d951dab72fb65da3df62e54a61f7f2ba
Alice Brown <alice@alum.mit.edu>
2022-04-13 15:55:25 -0400

    updated nokogiri
    and checked in schema from production

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_11_133519) do
-  create_table "pages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_bin", options: "ENGINE=MyISAM", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_233522) do
+  create_table "pages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "url"
     t.string "title"
     t.text "notes", size: :medium
@@ -26,22 +26,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_11_133519) do
     t.integer "stars", limit: 2, default: 10
     t.integer "ultimate_parent_id"
     t.integer "sanitize_version", limit: 2, default: 1, null: false
-    t.string "cached_tag_string", default: "", null: false
-    t.text "my_notes"
-    t.string "cached_hidden_string", default: "", null: false
+    t.text "my_notes", size: :medium
     t.string "type"
+    t.boolean "hidden", default: false
     t.index ["parent_id"], name: "index_pages_on_parent_id"
-    t.index ["size"], name: "index_pages_on_size", length: 250
+    t.index ["size"], name: "index_pages_on_size"
     t.index ["stars"], name: "index_pages_on_stars"
     t.index ["ultimate_parent_id"], name: "index_pages_on_ultimate_parent_id"
   end
 
-  create_table "pages_tags", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "pages_tags", id: false, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "page_id"
     t.integer "tag_id"
   end
 
-  create_table "tags", id: :integer, charset: "latin1", options: "ENGINE=MyISAM", force: :cascade do |t|
+  create_table "tags", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "type", default: "", null: false
     t.index ["name"], name: "tag_name"
