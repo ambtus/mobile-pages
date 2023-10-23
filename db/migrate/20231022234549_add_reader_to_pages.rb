ba1f11071baddf9566046962caf464e8de1feabf
Alice Brown <alice@alum.mit.edu>
2023-10-23 09:50:24 -0400

    oops?

--- db/migrate/20231022234549_add_reader_to_pages.rb
+++ db/migrate/20231022234549_add_reader_to_pages.rb
@@ -2,8 +2,4 @@ class AddReaderToPages < ActiveRecord::Migration[7.0]
   def change
     add_column :pages, :reader, :boolean, :default => false
   end
-  Reader.recache_all
-  Con.recache_all
-  Pro.recache_all
-  Hidden.recache_all
 end
