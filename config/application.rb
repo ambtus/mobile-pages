2151602772647289cdbba56c96843281d76563f2
Sidra <alice@alum.mit.edu>
2025-06-09 12:44:12 -0400

    tested 7.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+#  require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,11 +22,11 @@ Bundler.require(*Rails.groups)
 module MobilePages
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+   end
 end
