35e3151fe6cea028e60c066f766c6350e6da56b6
Sidra <alice@alum.mit.edu>
2025-06-10 10:40:39 -0400

    changed organization of features and cucumber options.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Log to STDOUT if asked
+  if ENV["LOUD"].present?
+    config.logger = ActiveSupport::Logger.new(STDOUT)
+      .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+      .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  end
+
 end
